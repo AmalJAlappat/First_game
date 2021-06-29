8130613a13baa23c5ea29a807f117393355326d9
Amal J Alappat <amalalappattofficial@gmail.com>
2021-06-29 13:51:49 +0530

    Step 4 completed

--- game.js
+++ game.js
@@ -1,10 +1,23 @@
 var gamePattern=[];
 var buttonColours=["red","blue","green","yellow"];
+var userClickedPattern=[];
+
+
+$("button").click(function(event){
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+});
+
 
 function nextSequence() {
   var randomNumber=Math.floor(Math.random()*4);
   var randomColour=buttonColours[nextSequence()];
+
   gamePattern.push(randomColour)
+
   $("#"+randomColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
   var sound=new Audio("./sounds/"+randomColour+".mp3");
+  sound.play();
+
 }
