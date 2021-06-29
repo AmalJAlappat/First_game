e377f6a6d1b727a0d4eb1dd04c3bdee180daed55
Amal J Alappat <amalalappattofficial@gmail.com>
2021-06-29 20:54:14 +0530

    errors fixed

--- game.js
+++ game.js
@@ -1,55 +1,70 @@
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 var userClickedPattern = [];
-var setKey = 0;
+var setKey = true;
 var level = 0;
 var clk = 1;
-var compareArr= true;
 
 // Detect Keypress
 $(document).keydown(function() {
-  if (setKey == 0) {
-    // $("#level-title").text("Level " + level);
+  if (setKey) {
     nextSequence();
   }
-  setKey = 1;
+  setKey =false;
 });
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+ checkAnswer((userClickedPattern.length)-1);
+});
+
+
 
 // Sequence generator
 function nextSequence() {
+  userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
   var randomColour = buttonColours[randomNumber];
 
-  gamePattern.push(randomColour)
+  gamePattern.push(randomColour);
 
   $("#" + randomColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomColour);
-  checkAnswer(level);
 }
 
 
 
 // checkAnswer
 function checkAnswer(currentLevel){
-  // Detect Click
-  while (level==clk) {
-    clk++;
-    $(".btn").click(function() {
-      var userChosenColour = $(this).attr("id");
-      userClickedPattern.push(userChosenColour);
-      playSound(userChosenColour);
-      animatePress(userChosenColour);
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
-    });
-  }
+        console.log("success");
 
-  if(compareArrays(gamePattern,userClickedPattern)){
-    nextSequence();
-  }
+        //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+        if (userClickedPattern.length === gamePattern.length){
+
+          //5. Call nextSequence() after a 1000 millisecond delay.
+          setTimeout(function () {
+            nextSequence();
+          }, 1000);
+
+        }
+
+      } else {
+
+        console.log("wrong");
+
+      }
+
 
 }
+
+
 
 // Sound player
 function playSound(name) {
@@ -67,14 +82,11 @@ function animatePress(currentColour) {
 
 // To check if arrays are equal
 
-function compareArrays(arr1,arr2) {
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i]==arr2[i]) {
-
-    }
-    else {
-       compareArr=false
-    }
-  }
-  return compareArr;
-}
+// function compareArrays(arr1,arr2) {
+//   if (arr1.toString()==arr2.toString()) {
+//     compareArr=true;
+//   } else {
+//     compareArr=false;
+//   }
+//   return compareArr;
+// }
